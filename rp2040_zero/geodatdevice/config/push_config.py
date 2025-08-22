26641898afe721e953ab03f4d47fcf6adae43e7c
TerminalGoat <74200268+TerminalGoat232@users.noreply.github.com>
2025-08-22 20:04:44 +0700

    config parser

--- rp2040_zero/geodatdevice/config/push_config.py
+++ rp2040_zero/geodatdevice/config/push_config.py
@@ -1,8 +1,16 @@
 import serial
 import serial.tools.list_ports as ls_ports
 import time 
-import pathlib
+import pathlib as pl
+import argparse as ap 
 import json
+
+parser = ap.ArgumentParser(
+    prog="push_config", 
+    description = "CLI key config pusher for GeodatDevice"
+)
+parser.add_argument("-c", "--config")
+args = parser.parse_args()
 
 def search_for_port():
     ports = ls_ports.comports()
@@ -14,12 +22,16 @@ port = search_for_port()
 
 if not port: raise RuntimeError("Where RP2040?")
 ser_rp = serial.Serial(port, 115200)
-time.sleep(2)
+
 print("Waiting...")
+time.sleep(1)
+
+def desired_config_file():
+    return 'config.json' if (args.config == "") else args.config
 
 while 1:
     print("ok buddy rp2040")
-    with open(pathlib.Path('config.json'), "r") as file:
+    with open(pl.Path(desired_config_file()), "r") as file:
         c = []
         dat = file.read()
         config_dat = json.loads(dat)
@@ -27,9 +39,8 @@ while 1:
             c.append(int(config_dat[f"K_{x}"], 16))
         msg_sr = bytes(c);
         ser_rp.write(msg_sr);
-        print(f"Successfully pushed key configuration to GeodatDevice: \n {msg_sr}")
+        print(f"Successfully pushed key configuration to GeodatDevice: \n {c}")
         ser_rp.close()
         break
-else: pass
 
 
